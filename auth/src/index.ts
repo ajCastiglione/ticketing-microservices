69975f75fca1622c3f44af9f06d0ebf94628f3e6
Antonio Castiglione <antonio@minervawebdevelopment.com>
2025-06-11 21:35:30 -0400

    Added mongoose and depl for it

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -27,6 +28,16 @@ app.all("*", async () => {
 // Middleware
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Auth service is running on port ${PORT}!!`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to mongo");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(PORT, () => {
+    console.log(`Auth service is running on port ${PORT}!!`);
+  });
+};
+
+start();
