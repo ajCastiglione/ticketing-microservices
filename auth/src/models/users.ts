bf7b932c8f7043e433c60fb6b4c4672261a62c05
Antonio Castiglione <antonio@minervawebdevelopment.com>
2025-06-12 21:53:19 -0400

    added logic for signin, refactored error middleware, updated PW class

--- auth/src/models/users.ts
+++ auth/src/models/users.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { Password } from "../services/password";
+import { PasswordManager } from "../services/password";
 
 // Interface that describes the properties that are required for a new User.
 // This interface is used to define the shape of the data that will be passed to the User Model when creating a new user.
@@ -21,20 +21,33 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    // This sort of mixes the "View" of MVC patterns, but is useful for microservices to set uniform payload structures.
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre("save", async function (done) {
   if (this.isModified("password")) {
-    const hashed = await Password.toHash(this.get("password"));
+    const hashed = await PasswordManager.toHash(this.get("password"));
     this.set("password", hashed);
   }
   done();
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,55 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { validateRequest } from "../middleware/validate-requests";
+import { User } from "../models/users";
+import { BadRequestError } from "../errors/bad-request-error";
+import { PasswordManager } from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("signin route");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid login credentials");
+    }
+
+    const passwordsMatch = await PasswordManager.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError("Invalid login credentials");
+    }
+
+    // Generate jwt
+    const userJWT = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY! // Allows TS to know that JWT_KEY is defined - done so in index.ts
+    );
+
+    // Store on session obj
+    req.session = {
+      jwt: userJWT,
+    };
+
+    res.status(200).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
