ad4c98eeb06d5bca377ec786278949f85be0a78b
Antonio Castiglione <antonio@minervawebdevelopment.com>
2025-06-13 20:02:50 -0400

    Added signout logic and extracted current user logic

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Testing hi");
+  // Clear the cookie by setting it to an empty value and a past expiration date
+  req.session = null;
+
+  res.send({ message: "Signed out successfully" });
 });
 
 export { router as signoutRouter };
