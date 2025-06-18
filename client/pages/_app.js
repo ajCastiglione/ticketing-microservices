55c1f73718815983ccadb2df2cc6aa1b69b29480
Antonio Castiglione <antonio@minervawebdevelopment.com>
2025-06-17 20:30:12 -0400

    Created header and signout routes, adjusted initProps

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,29 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import buildClient from "../api/build-client";
+import Header from "../components/Header";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function AppComponent({ Component, pageProps, currentUser }) {
+  return (
+    <>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
